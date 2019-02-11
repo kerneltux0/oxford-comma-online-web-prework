7806c2fddb6f5b9173969c5d90c86d7233374f26
Ryan Sise <kerneltux0@gmail.com>
2019-02-11 17:13:54 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
   if array.size>=3
-    array.insert(-2, "and")
-    string = array.join(",")
+    array.insert(-2, "and ")
+    string = array.join(", ")
     string.sub(/(.*),/, '\1')
   elsif array.size==2
     string = array.join(",")
